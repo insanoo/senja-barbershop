76e07ce0c65b4290ea2ff303067373dcdd16d7e4
insanoo <insaanjr@gmail.com>
2024-08-09 22:22:15 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+    'node_modules/preline/dist/*.js'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom': "url('src/img/logo2.jpg')"
+      },
+    },
+  },
+  plugins: [
+    require('preline/plugin')
+  ],
+}
