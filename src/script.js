499aa74fff59e32ca7fc0880ac1d7bdf6dcce21b
insanoo <insaanjr@gmail.com>
2024-08-10 12:10:25 +0700

    perbaikan src error script.js

--- src/script.js
+++ src/script.js
@@ -1,17 +1,16 @@
-//nav
 window.addEventListener('load', function() {
-    (function() {
-      const collapse = window?.$hsCollapseCollection ? HSCollapse.getInstance('#hs-header-scrollspy-collapse', true) : null;
-      const scrollSpy = window?.$hsScrollspyCollection ? HSScrollspy.getInstance('[data-hs-scrollspy="#scrollspy"]', true) : null;
+  (function() {
+    const collapse = window?.$hsCollapseCollection ? HSCollapse.getInstance('#hs-header-scrollspy-collapse', true) : null;
+    const scrollSpy = window?.$hsScrollspyCollection ? HSScrollspy.getInstance('[data-hs-scrollspy="#scrollspy"]', true) : null;
 
-      if (collapse && scrollSpy) scrollSpy.element.on('beforeScroll', () => {
-        console.log(1);
-        return new Promise((res) => {
-          if (window.innerWidth <= 639 && collapse.element.el.classList.contains('open')) {
-            collapse.element.hide();
-            HSStaticMethods.afterTransition(collapse.element.content, () => res(true));
-          } else res(true);
-        });
+    if (collapse && scrollSpy) scrollSpy.element.on('beforeScroll', () => {
+      console.log(1);
+      return new Promise((res) => {
+        if (window.innerWidth <= 639 && collapse.element.el.classList.contains('open')) {
+          collapse.element.hide();
+          HSStaticMethods.afterTransition(collapse.element.content, () => res(true));
+        } else res(true);
       });
-    })();
-  });
+    });
+  })();
+});
